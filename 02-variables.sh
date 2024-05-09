59de9b703c25d89c84662ad8e7e2612ced689277
Khyathi304 <khyathikamireddy04@gmail.com>
2024-05-09 19:51:12 +0100

    hello script

--- 02-variables.sh
+++ 02-variables.sh
@@ -4,4 +4,4 @@
 echo " Ramesh:: Hello Suresh, How are you?"
 echo " Suresh:: Hi Ramesh, I am fine, How are you?"
 echo " Ramesh:: I am fine too, How is your work?"
-echo "Suresh:: I am thinking to upgrade to DevOps"
+echo " Suresh:: I am thinking to upgrade to DevOps"
