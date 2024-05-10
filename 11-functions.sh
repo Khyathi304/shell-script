b05e14b5555a9d2379440ea3f29425e7a1a50772
Khyathi304 <khyathikamireddy04@gmail.com>
2024-05-10 20:38:17 +0100

    functions

--- 11-functions.sh
+++ 11-functions.sh
@@ -7,12 +7,12 @@ SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 VALIDATE(){
-    if [ $1 -eq 0 ]
+    if [ $1 -ne 0 ]
     then
-        echo "$2... Success"
+        echo "$2... Failure"
         exit 1
     else
-        echo "$2... Failure"
+        echo "$2... Success"
         fi
 }
 if [ $USERID -ne 0 ]
