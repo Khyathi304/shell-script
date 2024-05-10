c1e63e5690537b0b1358869070b8f4aca86be3d3
Khyathi304 <khyathikamireddy04@gmail.com>
2024-05-10 20:48:00 +0100

    functions

--- 12-colors.sh
+++ 12-colors.sh
@@ -15,10 +15,10 @@ echo "Scripts started running at : $TIMESTAMP"
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$2... $R Failure"
+        echo -e "$2... $R Failure $N"
         exit 1
     else
-        echo -e "$2... Success"
+        echo -e "$2... $G Success $N"
         fi
 }
 if [ $USERID -ne 0 ]
