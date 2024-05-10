b0471e62c09c3afd9130e40056fb6cd5f9e67419
Khyathi304 <khyathikamireddy04@gmail.com>
2024-05-10 21:20:02 +0100

    colors

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -2,6 +2,10 @@
 
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
 if [ $USERID -ne 0 ]
 then
 echo "Please run the script with root access"
@@ -15,4 +19,10 @@ fi
 for i in $@
 do
 echo "Package to install: $i"
+dnf list installed $i &>>LOGFILE
+if [ $? -eq 0 ]
+then
+echo "$i is installed.. Skipping"
+fi
+
 done
