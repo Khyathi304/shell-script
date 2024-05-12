9e69fccb4003363eab03867086fd3c3d3b99110b
Khyathi304 <khyathikamireddy04@gmail.com>
2024-05-12 12:12:22 +0100

    shell-script

--- 15-current-script.sh
+++ 15-current-script.sh
@@ -5,6 +5,6 @@ echo "Before calling another Sciprt :$COURSE"
 echo "Process ID of current script: $$"
 
 #./16-other-script.sh
-source./16-other-script.sh
+source ./16-other-script.sh
 
 echo "After calling other script: $COURSE"
