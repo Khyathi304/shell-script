833fdcaa348a83039f05175c84053c6f4dba078c
Khyathi304 <khyathikamireddy04@gmail.com>
2024-05-13 19:25:38 +0100

    shell-script

---
+++ 19-disk-usage.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep xfs )
+DISK_THRESHOLD=5
+
+while IFS = read -r line
+do 
+  USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1 )
+  FOLDER=$(echo $line | awk -F " " '{print $NF}')
+if [ $USAGE -gt 5 ]
+then
+echo "$FOLDER is more than $DISK_THRESHOLD, Usage: $USAGE"
+done <<<$DISK_USAGE
